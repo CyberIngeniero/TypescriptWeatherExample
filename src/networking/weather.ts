827997f08a7d0b1509cd0c9f4db49a4b12bcdd55
CyberIngeniero <n.pino@uc.cl>
2022-07-23 12:29:05 +0200

    Ajustes de espacios

--- src/networking/weather.ts
+++ src/networking/weather.ts
@@ -5,8 +5,7 @@ export const getWeather = async (city: string) :Promise<WeatherResponse> =>  {
         method: 'GET',
         redirect: 'follow'
     };
-    
+
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`, requestOptions);
     return response.json();
-} 
-
+}
